9902428838ed5e71645e3900d52aa99398b2b703
Paul Koop <94432949+pkoopongithub@users.noreply.github.com>
2024-08-08 16:06:49 +0200

    Add files via upload

---
+++ db_zeitreihe.php
@@ -0,0 +1,72 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Duesseldorfer Schuelerinventar Paul Koop M.A.</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="jquery/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <style type="text/css">
+.auto-style1 {
+	font-size: xx-large;
+	color: #FFFFFF;
+	background-color: #0000FF;
+}
+.auto-style3 {
+	background-color: #0000FF;
+}
+.auto-style6 {
+	color: #FFFFFF;
+	background-color: #0000FF;
+}
+.auto-style7 {
+	font-size: xx-small;
+}
+.auto-style8 {
+	color: #FFFFFF;
+}
+.auto-style9 {
+	font-size: medium;
+}
+.auto-style10 {
+	background-color: #FFFFFF;
+    
+    @media print {
+      * {
+        text-shadow: none !important;
+        color: #000 !important;
+        background: transparent !important;
+        box-shadow: none !important;
+        border-width:0 0px 0px 0 !important;
+        page-break-inside: avoid;
+      }
+}
+</style>
+</head>
+
+<body class="schrift">
+<? include("db_con_func.php"); ?>
+<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td valign=top class="auto-style10">
+
+<div class="panel panel-default">
+  <div class="panel-heading">D&uuml;SK</div>
+  <div class="panel-body">
+    Gruppenansicht D&uuml;sseldorfer Sch&uuml;lerinventar online
+  </div>
+</div>
+  <? include("zeitleistef1.php");  ?>
+<div class="panel panel-default">
+  <div class="panel-heading">Paul Koop M.A.</div>
+  <div class="panel-body">
+    Thomashofstrasse 19<br>
+    52070 Aachen
+  </div>
+</div>
+&nbsp;</td>
+  </tr>
+
+</table>
+</body>
+</html>
